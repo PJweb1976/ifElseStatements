98a1344a473312a2de07068ae0e4b6e2e6534d59
Peggy <info@klemtech.nl>
2021-12-08 20:05:12 +0100

    Comparison and Logical Operators

--- pub.js
+++ pub.js
@@ -1,16 +1,17 @@
 const age = 45,
 isFemale = true, 
-driverStatus = "bob";
-
-/*const age = 45,
-isFemale = true, 
-driverStatus = "bob";
-*/
+driverStatus = "bob",
+younAdult = "discount",
+firstName = "Luuk",
+totalAmount = 105
 
 
 let ageMsg = "Please stay outside",
 paymentMsg = "Please pay in full",
-transportMsg = "Must call cab"
+transportMsg = "Must call cab",
+allAgeMsg = "No discount",
+festiveMsg = "No free beer",
+anniversaryBillMsg = "No free stuff" 
 
 if (age >= 18) {
     ageMsg = "You are welcome";
@@ -29,5 +30,58 @@ if (driverStatus==="bob") {
 }
     console.log (transportMsg);
 
+if (age >= 18 && age <= 25) {
+    allAgeMsg = "You get 50% off";
+}    
+    console.log (allAgeMsg);
 
+if (firstName === "Sarah || Bram") {
+    festiveMsg = "You get free beer";
+}
+    console.log (festiveMsg);
 
+if (totalAmount > 25) {
+    anniversaryBillMsg = "Free portion bitterballen";
+}
+if (totalAmount > 50) {
+    anniversaryBillMsg = "Free portion of nachos";
+}
+if (totalAmount > 100) {
+    anniversaryBillMsg = "Free bottle of Champagne";
+}   
+    console.log (anniversaryBillMsg);
+
+/*
+const myAge = 20;
+
+let ageMsg = "you aint coming!"
+
+if (myAge >= 18 && myAge <= 30) {
+    ageMsg = "You are welcome";
+} 
+console.log (ageMsg);
+*/
+
+/*
+//omgekeerd
+const myAge = 20;
+
+let ageMsg = "You are welcome"
+
+if (myAge < 18 || myAge > 30) {
+    ageMsg = "you aint coming!";
+} 
+console.log (ageMsg);
+*/
+
+/*
+//omgekeerd met extra exit voor 25
+const myAge = 25;
+
+let ageMsg = "You are welcome"
+
+if (myAge < 18 || myAge > 30 || myAge === 25) {
+    ageMsg = "you aint coming!";
+} 
+console.log (ageMsg);
+*/
